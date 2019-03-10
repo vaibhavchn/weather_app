cb0b9f7b745b1124a84b32201faaaa22902e3b1a
vaibhavchn <vaibhav.chauhan.674@gmail.com>
2019-03-10 19:06:27 +0530

    commit for alibaba clone

--- weather_js.js
+++ weather_js.js
@@ -4,9 +4,12 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     const temperatureDegree = document.querySelector('.temperature-degree');
     const locationTimezone = document.querySelector('.location-timezone');
+    const getForC = document.querySelector('.degree-section span');
+    const getTempArea= document.querySelector('.degree-section');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
@@ -25,8 +28,24 @@ window.addEventListener('load', () => {
                     temperatureDescription.textContent = summary.toUpperCase();
                     locationTimezone.textContent = data.timezone;
 
+                    const tempCelsius = (temperature - 32)*(5/9);
+
                     const icon_class = document.querySelector('.icon');
                     setIcons(icon, icon_class);
+
+                    getTempArea.addEventListener('click', () => {
+
+                        if (getForC.textContent === "F"){
+                            getForC.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(tempCelsius);
+                        }
+                        else {
+                            getForC.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+
+                    });
+
                 });
         });
     }
@@ -38,4 +57,22 @@ window.addEventListener('load', () => {
         skycons.play();
         return skycons.set(iconId,  Skycons[currentIcon]);
     }
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
